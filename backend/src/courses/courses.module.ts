a307a45c6cf1eeebb484a229be476e186893abd8
Rahul Vijay Patil <rahulpatil096k@gmail.com>
2025-05-14 20:29:40 +0530

    Create courses.module.ts

---
+++ backend/src/courses/courses.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { CoursesController } from './courses.controller';
+import { SupabaseModule } from '../supabase/supabase.module';
+
+@Module({
+  imports: [SupabaseModule],
+  controllers: [CoursesController],
+  providers: [CoursesService],
+})
+export class CoursesModule {}
